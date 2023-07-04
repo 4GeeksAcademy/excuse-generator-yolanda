6433722d38affb1ad446c3ea5646dd33d0dfdaa9
Yolanda CM <102764721+Yaoska18Carrillo@users.noreply.github.com>
2023-07-04 02:20:29 +0000

    excuse generator

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function generate() {
+const generate = () => {
   let who = ["The dog", "My grandma", "His turtle", "My bird"];
   let action = ["ate", "peed", "crushed", "broke"];
   let what = ["my homework", "the keys", "the car"];
@@ -24,4 +24,4 @@ function generate() {
     " " +
     when[generate_when] +
     "</p> <button onclick='generate()'>Generate excuse</button>";
-}
+};
